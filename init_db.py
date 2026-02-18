d73ef4bc6f2923936089cbaead38c966047d56e0
Alpha-A-Bah <118969609+Alpha-A-Bah@users.noreply.github.com>
2026-02-18 10:04:06 +0000

    Initial commit - Flask app with db setup

---
+++ init_db.py
@@ -0,0 +1,22 @@
+import sqlite3
+
+connection = sqlite3.connect("database.db")
+cursor = connection.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS vehicles (
+               id INTEGER PRIMARY KEY AUTOINCREMENT,
+               reg TEXT NOT NULL,
+               vin TEXT NOT NULL,
+               model TEXT NOT NULL,
+               status TEXT,
+               current_user TEXT,
+               current_mileage INTEGER,
+               last_checkin TEXT,
+               last_checkout TEXT,
+                     
+               )
+""")
+
+connection.commit()
+connection.close()
